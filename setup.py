e6b966600c61c3913b2f76d28d87143f5b6d2f0c
avinassh <avinashsajjan@gmail.com>
2014-10-09 22:03:31 +0530

    updated setup.py file

--- setup.py
+++ setup.py
@@ -5,15 +5,39 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open('README.rst') as f:
+    long_description = f.read('README.rst')
+
+version = '0.1.0'
+
 setup(
     name='haxor',
-    version='0.0.1',
-    packages=find_packages(),
-    include_package_data=True,
-    url='https://github.com/avinassh/haxor/',
-    license='MIT',
+    version=version,
+    install_requires=requirements,
     author='Avinash Sajjanshetty',
     author_email='a@sajjanshetty.com',
-    description='Unofficial Python wrapper for official Hacker News API',
-    install_requires=requirements
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='tests',
+    url='https://github.com/avinassh/haxor/',
+    license='MIT',
+    description='Unofficial Python wrapper for Hacker News API',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
 )
