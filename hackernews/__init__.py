1557cacc619786e81d9afa690b4b7cb51ad27d9f
avi <hi@avi.im>
2017-09-09 19:22:52 +0530

    Add a working version of asyncio

--- hackernews/__init__.py
+++ hackernews/__init__.py
@@ -13,8 +13,11 @@ from __future__ import unicode_literals
 import datetime
 import json
 import sys
+from urllib.parse import urljoin
 
 import requests
+import aiohttp
+import asyncio
 
 from .settings import supported_api_versions
 
@@ -43,6 +46,17 @@ class HTTPError(Exception):
     pass
 
 
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def fetch_all(session, urls, loop):
+    tasks = [asyncio.ensure_future(fetch(session, url)) for url in urls]
+    results = await asyncio.gather(*tasks)
+    return results
+
+
 class HackerNews(object):
 
     def __init__(self, version='v0'):
@@ -59,13 +73,22 @@ class HackerNews(object):
             self.base_url = supported_api_versions[version]
         except KeyError:
             raise InvalidAPIVersion
+        self.item_url = urljoin(self.base_url, 'item/')
+        self.user_url = urljoin(self.base_url, 'user/')
         self.session = requests.Session()
 
-    def _get(self, url):
+    def _get_sync(self, url):
+        response = self.session.get(url)
+        if response.status_code == requests.codes.ok:
+            return response.json()
+        else:
+            raise HTTPError
+
+    def _get_async(self, urls):
         """Internal method used for GET requests
 
         Args:
-            url (string): URL to send GET.
+            urls (list): List of URLs to fetch.
 
         Returns:
             requests' response object
@@ -74,20 +97,55 @@ class HackerNews(object):
           HTTPError: If HTTP request failed.
 
         """
-        response = self.session.get(url)
-        if response.status_code == requests.codes.ok:
-            return response
-        else:
-            raise HTTPError
+        loop = asyncio.get_event_loop()
+        with aiohttp.ClientSession(loop=loop) as session:
+            results = loop.run_until_complete(fetch_all(session, urls, loop))
+        return [json.loads(r) for r in results]
 
-    def _get_page(self, page):
-        return self._get('{0}{1}.json'.format(self.base_url, page))
+    def get_user_by_ids(self, user_ids):
+        """
+        Given a list of user ids, return all the User objects
+        """
+        urls = [urljoin(self.user_url, F"{i}.json") for i in user_ids]
+        result = self._get_async(urls=urls)
+        return [User(r) for r in result]
 
-    def _get_page_param(self, page, param):
-        return self._get('{0}{1}/{2}.json'.format(self.base_url, page, param))
+    def get_story_by_ids(self, story_ids):
+        """
+        Given a list of story ids, return all the stories Item object
+        """
+        urls = [urljoin(self.item_url, F"{i}.json") for i in story_ids]
+        result = self._get_async(urls=urls)
+        return [Item(r) for r in result]
+
+    def _get_stories(self, page, limit):
+        """
+        Hacker News has different categories (i.e. stories) like
+        'topstories', 'newstories', 'askstories', 'showstories', 'jobstories'.
+        This method, first fetches the relevant story ids of that category
+
+        The URL is: https://hacker-news.firebaseio.com/v0/<story_name>.json
+
+        e.g. https://hacker-news.firebaseio.com/v0/topstories.json
+
+        Then, asynchronously it fetches each story and returns the Item objects
+
+        The URL for individual story is:
+            https://hacker-news.firebaseio.com/v0/item/<item_id>.json
+
+        e.g. https://hacker-news.firebaseio.com/v0/item/69696969.json
+        """
+        url = urljoin(self.base_url, F"{page}.json")
+        story_ids = self._get_sync(url)[:limit]
+        return self.get_story_by_ids(story_ids=story_ids)
 
     def get_item(self, item_id):
         """Returns Hacker News `Item` object.
+
+        Fetches the data from url:
+            https://hacker-news.firebaseio.com/v0/item/<item_id>.json
+
+        e.g. https://hacker-news.firebaseio.com/v0/item/69696969.json
 
         Args:
             item_id (int or string): Unique item id of Hacker News story,
@@ -101,7 +159,8 @@ class HackerNews(object):
 
         """
 
-        response = self._get_page_param('item', item_id).json()
+        url = urljoin(self.item_url, F"{item_id}.json")
+        response = self._get_sync(url)
 
         if not response:
             raise InvalidItemID
@@ -110,6 +169,11 @@ class HackerNews(object):
 
     def get_user(self, user_id):
         """Returns Hacker News `User` object.
+
+        Fetches data from the url:
+            https://hacker-news.firebaseio.com/v0/user/<user_id>.json
+
+        e.g. https://hacker-news.firebaseio.com/v0/user/pg.json
 
         Args:
             user_id (string): unique user id of a Hacker News user.
@@ -121,7 +185,8 @@ class HackerNews(object):
           InvalidUserID: If no such user exists on Hacker News.
 
         """
-        response = self._get_page_param('user', user_id).json()
+        url = urljoin(self.user_url, F"{user_id}.json")
+        response = self._get_sync(url)
 
         if not response:
             raise InvalidUserID
@@ -137,7 +202,7 @@ class HackerNews(object):
         Returns:
             `list` object containing ids of top stories.
         """
-        return self._get_page('topstories').json()[:limit]
+        return self._get_stories('topstories', limit)
 
     def new_stories(self, limit=None):
         """Returns list of item ids of current new stories
@@ -148,7 +213,7 @@ class HackerNews(object):
         Returns:
             `list` object containing ids of new stories.
         """
-        return self._get_page('newstories').json()[:limit]
+        return self._get_stories('newstories', limit)
 
     def ask_stories(self, limit=None):
         """Returns list of item ids of latest Ask HN stories
@@ -159,7 +224,7 @@ class HackerNews(object):
         Returns:
             `list` object containing ids of Ask HN stories.
         """
-        return self._get_page('askstories').json()[:limit]
+        return self._get_stories('askstories', limit)
 
     def show_stories(self, limit=None):
         """Returns list of item ids of latest Show HN stories
@@ -170,7 +235,7 @@ class HackerNews(object):
         Returns:
             `list` object containing ids of Show HN stories.
         """
-        return self._get_page('showstories').json()[:limit]
+        return self._get_stories('showstories', limit)
 
     def job_stories(self, limit=None):
         """Returns list of item ids of latest Job stories
@@ -181,19 +246,30 @@ class HackerNews(object):
         Returns:
             `list` object containing ids of Job stories.
         """
-        return self._get_page('jobstories').json()[:limit]
+        return self._get_stories('jobstories', limit)
 
     def updates(self):
         """Returns list of item ids and user ids that have been
         changed/updated recently.
 
+        Fetches data from URL:
+            https://hacker-news.firebaseio.com/v0/updates.json
+
         Returns:
             `dict` with two keys whose values are `list` objects
         """
-        return self._get_page('updates').json()
+        url = urljoin(self.base_url, 'updates.json')
+        response = self._get_sync(url)
+        return {
+            'items': self.get_story_by_ids(story_ids=response['items']),
+            'profiles': self.get_user_by_ids(user_ids=response['profiles'])
+        }
 
     def get_max_item(self):
         """Returns list of item ids of current top stories
+
+        Fetches data from URL:
+            https://hacker-news.firebaseio.com/v0/maxitem.json
 
         Args:
             limit (int): specifies the number of stories to be returned.
@@ -201,7 +277,8 @@ class HackerNews(object):
         Returns:
             `int` if successful.
         """
-        return self._get_page('maxitem').json()
+        url = urljoin(self.base_url, 'maxitem.json')
+        return self._get_sync(url)
 
 
 class Item(object):
@@ -216,9 +293,7 @@ class Item(object):
         self.item_type = data.get('type')
         self.by = data.get('by')
         self.submission_time = datetime.datetime.fromtimestamp(
-            data.get(
-                'time',
-                0))
+            data.get('time', 0))
         self.text = data.get('text')
         self.dead = data.get('dead')
         self.parent = data.get('parent')
