c2bcd44d8a59a8fa01245f0f7abc60cab1f820b4
Ido Nov <idonov8@gmail.com>
2022-03-06 17:20:54 +0200

    Support running not in main thread
    This is required to make it work in a cloud function

--- hackernews/__init__.py
+++ hackernews/__init__.py
@@ -136,7 +136,13 @@ class HackerNews(object):
             results (obj): All URL requests' responses
 
         """
+        try:
         loop = asyncio.get_event_loop()
+        except RuntimeError as ex:
+            if "There is no current event loop in thread" in str(ex):
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+                return asyncio.get_event_loop()
         results = loop.run_until_complete(self._async_loop(urls))
         return results
 
