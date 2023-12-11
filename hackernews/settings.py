f1a24228e151149940e15714fad3e78b70964b6f
Avinash Sajjanshetty <opensource@avi.im>
2023-12-11 17:00:47 +0530

    reset

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -1,2 +1,1 @@
-supported_api_versions = {'v0': 'https://hacker-news.firebaseio.com/v0/',
-                          'v1': 'https://hacker-news.firebaseio.com/v0/'}
+supported_api_versions = {'v0': 'https://hacker-news.firebaseio.com/v0/'}
